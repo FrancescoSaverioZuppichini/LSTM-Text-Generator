aabee421455b60483771a63f7fc5d70efda900ef
FrancescoSaverioZuppichini <zuppif@usi.ch>
2017-10-31 11:18:31 +0100

    re changed reader

--- core/reader/Reader.py
+++ core/reader/Reader.py
@@ -1,13 +1,17 @@
 # import nltk
 from random import randint
+import numpy as np
 
 class Reader():
 
-    def __init__(self):
+    def __init__(self, sequence_len=64,batch_size=32):
         self.data_set = {}
         self.data_set_inv = {}
         self.data = []
-        self.pointer = {'input': 0, 'target':0, 'prev':0 , 'step': 0}
+        # self.pointer = {'input': 0, 'target':0, 'prev':0 , 'step': 0}
+        self.sequence_len = sequence_len
+        self.batch_size = batch_size
+        self.training = {'X' : [], 'Y' :[]}
 
     def read(self, path, encode_words=False):
         """
@@ -31,83 +35,92 @@ class Reader():
         self.data_set = {k: v for k,v in enumerate(unique_chars)}
         self.data_set_inv = {k: v for v,k in enumerate(unique_chars)}
 
+        self.data_encoded = [self.encode(char) for char in self.data]
+
+    def create_set(self, data, start, end, step):
+        len_data = len(self.data)
+
+        S = np.array([data[start:len_data - end]])
+        S = S.T
+
+        S = S[:len_data//step * step]
+
+        return S.reshape([-1,step])
+
+
+    def create_training_set(self):
+        step  = self.sequence_len
+
+        X, Y = self.create_set(self.data_encoded, 0, 1, step), self.create_set(self.data_encoded, 1, 0, step)
+
+        self.training = {'X' : X, 'Y' :Y}
+
+        return X, Y
+
+    def decode_array(self, array):
+
+        return [self.decode(index) for index in array]
+
     def decode(self,index):
         return self.data_set[index]
 
     def encode(self,word):
         return self.data_set_inv[word]
 
-    def init_batch(self,input,target, random=False, step=1):
-
-        if(input > len(self.data)):
-            raise Exception('Input size cannot be larger than the data set.')
-
-        self.pointer['input'] = input
-        self.pointer['target'] = target
-        self.pointer['prev'] = 0
-        self.pointer['step'] = step
-
-        if(random):
-            self.pointer['prev'] = randint(0, len(self.data) - input - target - 1)
-
-    def compute_lower_upper(self, random=False, init=False):
-        p = self.pointer['prev']
-        i = self.pointer['input']
-        t = self.pointer['target']
-
-        if(init):
-            p = self.pointer['prev'] = 0
-        if (random):
-            self.pointer['prev'] = randint(0, len(self.data) - i - t)
-
-        lower = p + i
-        upper = p + 1 + t
-
-        return p,lower, upper
-
-    def next(self):
-        p = self.pointer['prev']
-
-        if p + self.pointer['input'] + 1 >= len(self.data):
-            p = self.pointer['prev'] = 0
-
-        upper = p + self.pointer['input'] + 1
-
-        raw = self.data[p:upper]
-
-        inputs, targets = raw[:len(raw) - 1], raw[1:]
-
-        self.pointer['prev'] = p + upper
-
-        return inputs, targets
-
-
-    # def next(self, random=False):
+    # def init_batch(self,input,target, random=False, batch_size=1):
     #
-    #     p,lower, upper = self.compute_lower_upper()
+    #     if(input > len(self.data)):
+    #         raise Exception('Input size cannot be larger than the data set.')
     #
-    #     if upper > len(self.data):
-    #         # go back
-    #         print('finish data')
-    #         p, lower, upper = self.compute_lower_upper(init=True,random=random)
+    #     self.pointer['input'] = input
+    #     self.pointer['target'] = target
+    #     self.pointer['prev'] = 0
+    #     self.pointer['step'] = step
     #
-    #     inputs, targets = ( self.data[p: lower], self.data[p + 1: upper] )
+    #     if(random):
+    #         self.pointer['prev'] = randint(0, len(self.data) - input - target - 1)
     #
-    #     self.pointer['prev'] = self.pointer['prev'] + self.pointer['step']
+    #     self.create_training_set()
+
+    def next(self, p):
+        X = self.training['X']
+        Y = self.training['Y']
+
+        return X[p: p + self.batch_size], Y[p : p + self.batch_size]
+    # def next(self):
+    #     p = self.pointer['prev']
+    #
+    #     if p + self.pointer['input'] + 1 >= len(self.data):
+    #         print('read dataset')
+    #         p = self.pointer['prev'] = 0
+    #
+    #     upper = p + self.pointer['input'] + 1
+    #
+    #     raw = self.data_encoded[p:upper]
+    #
+    #     inputs, targets = raw[:len(raw) - 1], raw[1:]
+    #
+    #     self.pointer['prev'] = p +  self.pointer['input'] + 1
     #
     #     return inputs, targets
 
     def get_unique_words(self):
         return len(self.data_set.keys())
 # little test
-
-r = Reader()
-r.read('../gesu_2.txt',encode_words=False)
 # #
-r.init_batch(3,3,step=3)
-# #
-for _ in range(20):
-    (i,t) = r.next()
-# #
-    print(i,t)
-    # print(len(i), len(t))
+# r = Reader(sequence_len=5,batch_size=2)
+# r.read('../gesu_2.txt',encode_words=False)
+# # # #
+# X, Y = r.create_training_set()
+# print(''.join(r.decode_array(X[2])))
+# print(X.shape)
+# print(len(r.data))
+# for _ in range(10):
+#     for i in range(len(X)//2):
+#         print(r.next(i))
+# # #
+# for _ in range(20):
+#     (i,t) = r.next()
+# # #
+#     print(i,t)
+#     # print(len(i), len(t))
--- core/train_2.py
+++ core/train_2.py
@@ -12,33 +12,36 @@ import sys
 
 FILE_NAME = './dio.txt'
 
-r = Reader.Reader()
-r.read(FILE_NAME,encode_words=False)
+CHECK_POINT = False
 
-batch_size = 32
-n_classes = r.get_unique_words()
+
+
+# variables
+batch_size = 64
+# sequence len
 n_timesteps = 256
 n_target = 256
-n_input = 256
-n_rnn = 256
+n_input = n_target
+# size of each rnn
+n_rnn = [256, 128]
 n_layers = 1
 
 learning_rate = 0.001
 
-# if(len(sys.argv) == 2):
-#     epochs = sys.argv[1]
+r = Reader.Reader(batch_size=batch_size, sequence_len=n_timesteps)
+r.read(FILE_NAME,encode_words=False)
+n_classes = r.get_unique_words()
+
 x = tf.placeholder(tf.float32, [batch_size, n_timesteps, n_classes])
 y = tf.placeholder(tf.float32,[batch_size, n_timesteps, n_classes])
 
 sess = tf.InteractiveSession()
 
-r.init_batch(n_timesteps, n_timesteps,step=n_timesteps)
-
-pred, cost, train_step, accuracy = model.RNN(x, y, n_timesteps, n_rnn, n_classes, batch_size)
+pred, cost, train_step, accuracy, correct_pred = model.RNN(x, y, n_timesteps, n_rnn, n_classes, batch_size)
 
 sess.run(tf.global_variables_initializer())
 
-# saver = tf.train.Saver()
+saver = tf.train.Saver()
 
 losses = []
 accuracies = []
@@ -46,13 +49,9 @@ accuracies = []
 total_loss = 0
 total_acc = 0
 
-n_batches = len(r.data)//batch_size
+n_batches = len(r.data)//(batch_size * n_timesteps)
 
-
-print(n_batches)
-start_time = time.clock()
-
-epochs = 1000
+epochs = 60
 
 print('-------------')
 print("Starting, {} epochs.".format(epochs))
@@ -60,51 +59,49 @@ print("Data size {}".format(len(r.data)))
 print("N classes {}".format(n_classes))
 print("N batches {}".format(n_batches))
 
+start_time = time.clock()
+X,Y = r.create_training_set()
+
 for n in range(epochs):
 
-    # for i in range(n_batches):
-        X = []
-        Y = []
-        for _ in range(batch_size):
-            inputs, targets = r.next()
+        for i in range(len(X)//batch_size):
+            x_batch,y_batch = r.next(i)
 
-            X.append([r.encode(input) for input in inputs])
+            x_hot = tf.one_hot(x_batch, depth=n_classes, on_value=1.0)
+            y_hot = tf.one_hot(y_batch, depth=n_classes, on_value=1.0)
 
-            Y.append([r.encode(target) for target in targets])
+            sess.run([ train_step ], feed_dict={x: x_hot.eval(), y: y_hot.eval()})
 
-            # TODO we can actually just build the X and shift it
-            #  by one to create the targets ;)
-        # X = np.array(X)
-        # Y = np.array(Y)
-        # print(X.shape, Y.shape)
-        x_hot = tf.one_hot(X, depth=n_classes, on_value=1.0)
-        y_hot = tf.one_hot(Y, depth=n_classes, on_value=1.0)
-        # print(n)
-        output, loss, acc = sess.run([ train_step, cost, accuracy ], feed_dict={x: x_hot.eval(), y: y_hot.eval()})
 
-        # losses.append(loss)
-        # accuracies.append(acc)
-        #
+        loss, acc = sess.run([cost, accuracy ], feed_dict={x: X, y: Y})
+
         total_loss += loss
         total_acc += acc
 
-        if(n % 10 == 0 and n > 0):
-            print('--------')
-            print('Iterations: {}'.format(n))
-            print('AVG Loss: {0:.4f}'.format(total_loss/(10)))
-            print('AVG Acc: {0:.4f}'.format(total_acc/(10)))
-            total_loss = 0
-            total_acc = 0
-            # save_path = saver.save(sess, "/tmp/model-{}.ckpt".format(time.strftime("%H:%M:%S")))
-            # print("Model saved in file: %s" % save_path)
-        # if(n % n_batches == 0):
-            # read all the dataset
-            # print('---------------')
-            # print('Iterations {}'.format(n))
-            # r.init_batch(n_timesteps,n_timesteps,step=n_timesteps)
+        # if(n % 10 == 0 and n > 0):
+        print('--------')
+        print('Iterations: {}'.format(n))
+        print('AVG Loss: {0:.4f}'.format(total_loss/(10)))
+        print('AVG Acc: {0:.4f}'.format(total_acc/(10)))
+        # print('Iter per second: {0:.4f}'.format(time.clock() - start_time_inner / 10/1000))
+        # start_time_inner = time.clock()
 
-# save_path = saver.save(sess, "/tmp/model.ckpt")
-# print("Model saved in file: %s" % save_path)
+        total_loss = 0
+        total_acc = 0
+
+        if(CHECK_POINT):
+            save_path = saver.save(sess, "/tmp/model-{}.ckpt".format(time.strftime("%H:%M:%S")))
+            print("Model saved in file: %s" % save_path)
+#
+# x_hot = tf.one_hot(X, depth=n_classes, on_value=1.0)
+#
+# ps = sess.run(pred,feed_dict={x: x_hot.eval()})
+# print(int(tf.argmax(pred, 1).eval()))
+# print(ps)
+# print(''.join([chr(p) for p in ps]))
+
+save_path = saver.save(sess, "/tmp/model.ckpt")
+print("Model saved in file: %s" % save_path)
 
 N_TEXT = 100
 
@@ -114,10 +111,6 @@ total_time = finish_time - start_time
 
 print('----------')
 print('Finish After: {0:.4f}s'.format(total_time))
-print('Epoches per second: {0:.4f}'.format(total_time/n))
+print('Iterations per second: {0:.4f}'.format(total_time/n))
 
 exit(1)
-# plt.plot(losses, label="losses")
-# plt.plot(accuracies, label="accuracies")
-# plt.legend()
-# plt.show()
